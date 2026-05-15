635fd200bdfff592e8e71a0f99a0aaa4ce86404f
HarshK213 <hkushwaha21305@gmail.com>
2026-05-15 14:30:33 +0530

    add particles background

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,40 @@ import Navbar from "./components/Navbar";
 import Skill from "./section/Skill";
 import Project from "./section/Project";
 import ContactMe from "./section/ContactMe";
-import Experience from "./section/Experience";
 import Resume from "./section/Resume";
 import Footer from "./components/Footer";
+import Particles from "./components/ui/particles.background";
 
 function App() {
 	return (
 		<div className="relative overflow-x-hidden">
-			<div className="fixed top-0 left-0 w-full z-50">
-				<Navbar />
+			<div className="fixed inset-0 -z-10">
+				<Particles
+					className="fixed inset-0 -z-10 h-screen w-screen pointer-events-none"
+					particleCount={200}
+					particleSpread={10}
+					speed={0.1}
+					particleColors={["#ffffff"]}
+					alphaParticles={false}
+					particleBaseSize={100}
+					sizeRandomness={1}
+					cameraDistance={20}
+					pixelRatio={1}
+				/>
+				<div className="absolute inset-0 bg-black/50" />
 			</div>
-			<Hero />
-			<About />
-			<Skill />
-			<Project />
-			<Experience />
-			<Resume />
-			<ContactMe />
-			<Footer />
+			<div className="relative z-10">
+				<div className="fixed top-0 left-0 w-full z-50 bg-transparent">
+					<Navbar />
+				</div>
+				<Hero />
+				<About />
+				<Skill />
+				<Project />
+				<Resume />
+				<ContactMe />
+				<Footer />
+			</div>
 		</div>
 	);
 }
